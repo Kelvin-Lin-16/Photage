8a91235b0b709824ca0e1b9a564de224197e153a
Kelvin Lin <fenglam09@gmail.com>
2016-04-30 09:20:31 -1000

    Basic layouts and buttons

---
+++ Photage/Photage/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Photage
+//
+//  Created by Kelvin Lam on 4/30/16.
+//  Copyright © 2016 Lins. All rights reserved.
+//
+
+import Foundation
+
+class User:NSObject{
+    
+}
--- Photage/Photage/ViewController.swift
+++ Photage/Photage/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func didTapAuth(sender: AnyObject) {
+    }
+    
+    @IBAction func didTapFetch(sender: AnyObject) {
+    }
+    
+    @IBAction func didTapCrop(sender: AnyObject) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
